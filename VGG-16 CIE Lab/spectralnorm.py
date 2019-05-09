0877baa2826402c6aab240c8bc1e6562e89d2bc9
Yuzhi ZHAO <531708015@qq.com>
2019-05-09 16:29:55 +0800

    Update spectralnorm.py

--- VGG-16 CIE Lab/spectralnorm.py
+++ VGG-16 CIE Lab/spectralnorm.py
@@ -6,54 +6,11 @@ Created on Thu Dec 27 13:24:11 2018
 """
 
 import torch
-from torch.optim.optimizer import Optimizer, required
-
-from torch.autograd import Variable
-import torch.nn.functional as F
 from torch import nn
-from torch import Tensor
 from torch.nn import Parameter
 
-def l2normalize(v, eps=1e-12):
+def l2normalize(v, eps = 1e-12):
     return v / (v.norm() + eps)
-
-class Self_Attn(nn.Module):
-    """ Self attention Layer"""
-    def __init__(self, in_dim):
-        super(Self_Attn, self).__init__()
-        self.chanel_in = in_dim
-        
-        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size = 1)
-        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size = 1)
-        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size = 1)
-        self.gamma = nn.Parameter(torch.zeros(1))
-
-        self.softmax  = nn.Softmax(dim = -1)
-    def forward(self, x):
-        """
-            inputs :
-                x : input feature maps(B X C X H X W)
-            returns :
-                out : self attention value + input feature 
-                attention: B X N X N (N is Height * Width)
-        """
-        batchsize, C, height, width = x.size()
-        # proj_query: reshape to B x N x C, N = H x W
-        proj_query  = self.query_conv(x).view(batchsize, -1, height * width).permute(0, 2, 1)
-        # proj_query: reshape to B x C x N, N = H x W
-        proj_key =  self.key_conv(x).view(batchsize, -1, height * width)
-        # transpose check, energy: B x N x N, N = H x W
-        energy =  torch.bmm(proj_query, proj_key)
-        # attention: B x N x N, N = H x W
-        attention = self.softmax(energy)
-        # proj_value is normal convolution, B x C x N
-        proj_value = self.value_conv(x).view(batchsize, -1, height * width)
-        # out: B x C x N
-        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
-        out = out.view(batchsize, C, height, width)
-        
-        out = self.gamma * out + x
-        return out
 
 class SpectralNorm(nn.Module):
     def __init__(self, module, name='weight', power_iterations=1):
