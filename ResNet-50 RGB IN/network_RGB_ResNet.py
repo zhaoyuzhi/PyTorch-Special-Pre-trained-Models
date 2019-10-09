6208207ff8b3204c0fffb6bccf1b9e765d57a6c5
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-10-09 19:59:25 +0800

    Update network_RGB_ResNet.py

--- ResNet-50 RGB IN/network_RGB_ResNet.py
+++ ResNet-50 RGB IN/network_RGB_ResNet.py
@@ -38,7 +38,7 @@ class BasicBlock(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace = True)
+        self.lrelu = nn.LeakyReLU(0.2, inplace = True)
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
@@ -48,7 +48,7 @@ class BasicBlock(nn.Module):
         identity = x
         out = self.conv1(x)
         out = self.bn1(out)
-        out = self.relu(out)
+        out = self.lrelu(out)
         out = self.conv2(out)
         out = self.bn2(out)
         if self.downsample is not None:
